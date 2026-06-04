db4cce92312d32468d78d75b3fff08f87f25d857
contactskinghost34500 <contactskinghost@gmail.com>
2026-06-04 15:29:54 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Ghostify from './GhostifyAdmin.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ghostify />
+  </React.StrictMode>
+)
